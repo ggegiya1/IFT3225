63a8b57431893429255cb93a0947c47bb48a54fa
Georgiy Gegiya <gia@KanotixBox.(none)>
2017-07-20 22:55:39 -0400

    use jquery and bootstrap

--- 2048/js/game.js
+++ 2048/js/game.js
@@ -2,11 +2,11 @@ function Game(rows){
     this.rows = rows;
 }
 
-Game.prototype.drawField = function(){
+Game.prototype.drawBoard = function(){
     var table = "<table>";
-    for (var i=0; i < rows; i++){
+    for (var i=0; i < this.rows; i++){
         table += "<tr>";
-        for (var j=0; j < rows; j++){
+        for (var j=0; j < this.rows; j++){
             table += "<td>";
             table += (i + j);
             table += "</td>";
@@ -14,5 +14,14 @@ Game.prototype.drawField = function(){
         table += "</tr>";
     }
     table += "</table";
-    document.getElementById("game_field").innerHTML = table;
+    $("#game_board").html(table);
 };
+
+
+$(document).ready(function(){
+    $("#start_btn").click(function(){
+        //alert("Start new game with " + $("#num_rows").val() + " rows");
+        var game = new Game($("#num_rows").val());
+        game.drawBoard();
+    });
+});
